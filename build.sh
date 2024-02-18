6254f300abe7fd916b54f57043e5f5ac6c177b09
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2024-02-18 14:21:20 -0500

    aaa

--- build.sh
+++ build.sh
@@ -6,12 +6,14 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Revertir las migraciones
+python manage.py migrate cuentas zero --database=default
+python manage.py migrate logs zero --database=logsdb
+
+# Aplicar las migraciones nuevamente
 python manage.py migrate cuentas --database=default
 python manage.py migrate logs --database=logsdb
 
-PGPASSWORD=sFwZbJ6SOkdh99N71nva530qw6aggiD4 psql -h dpg-cn94jb6d3nmc73dgl240-a.oregon-postgres.render.com -U chaucheritadb_user chaucheritadb -c "ALTER TABLE auth_user ALTER COLUMN last_login DROP NOT NULL;"
-
+# Crear un superusuario
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin2023')" | python manage.py shell
-
-PGPASSWORD=sFwZbJ6SOkdh99N71nva530qw6aggiD4 psql -h dpg-cn94jb6d3nmc73dgl240-a.oregon-postgres.render.com -U chaucheritadb_user chaucheritadb -c "ALTER TABLE auth_user ALTER COLUMN last_login SET NOT NULL;"
+WORD=sFwZbJ6SOkdh99N71nva530qw6aggiD4 psql -h dpg-cn94jb6d3nmc73dgl240-a.oregon-postgres.render.com -U chaucheritadb_user chaucheritadb -c "ALTER TABLE auth_user ALTER COLUMN last_login SET NOT NULL;"
