5f181e07e5276939582781886d6bea0393195a2a
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2024-02-18 14:07:00 -0500

    sa

--- build.sh
+++ build.sh
@@ -9,3 +9,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate cuentas --database=default
 python manage.py migrate logs --database=logsdb
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin2023')" | python manage.py shell
