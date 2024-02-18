78ced155244e481df276a240f82b77fef46e5283
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2024-02-18 14:28:16 -0500

    test

--- build.sh
+++ build.sh
@@ -6,14 +6,7 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
-# Revertir las migraciones
-python manage.py migrate cuentas zero --database=default
-python manage.py migrate logs zero --database=logsdb
-
-# Aplicar las migraciones nuevamente
+python manage.py collectstatic --no-input
 python manage.py migrate cuentas --database=default
 python manage.py migrate logs --database=logsdb
 
-# Crear un superusuario
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin2023')" | python manage.py shell
-WORD=sFwZbJ6SOkdh99N71nva530qw6aggiD4 psql -h dpg-cn94jb6d3nmc73dgl240-a.oregon-postgres.render.com -U chaucheritadb_user chaucheritadb -c "ALTER TABLE auth_user ALTER COLUMN last_login SET NOT NULL;"
