908a3b38c5a55d17d26402f632ce9d518893c139
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2024-02-19 07:28:19 -0500

    stataica

--- michaucheritaweb/settings.py
+++ michaucheritaweb/settings.py
@@ -133,12 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 if not DEBUG:
     # Directorio donde se recopilarán los archivos estáticos durante la fase de producción
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     # Almacenamiento de archivos estáticos para producción
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
