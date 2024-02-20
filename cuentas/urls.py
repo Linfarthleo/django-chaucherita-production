04d29b783d078d4febc1d03348413fa0ede3b3d5
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2024-02-20 09:37:00 -0500

    test

--- cuentas/urls.py
+++ cuentas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .views import CuentaView, MovimientoView
 
 urlpatterns = [
     path("", views.index),
-    path("cuentas/", views.CuentaView.as_view()),
-    path("movimientos/", views.MovimientoView.as_view()),
+    path("cuentas/", CuentaView.as_view()),
+    path("movimientos/", MovimientoView.as_view()),
 ]
