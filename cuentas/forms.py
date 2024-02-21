6eaee7318c6a1a11012e2a051761a882f0a1da73
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2024-02-20 21:51:46 -0500

    Xss prevention

--- cuentas/forms.py
+++ cuentas/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.html import escape
 
 from cuentas.models import Cuenta
 
@@ -14,3 +15,66 @@ class CrearCuentaForm(forms.Form):
         elif '<script>' in nombre_cuenta:
             raise forms.ValidationError('El nombre de la cuenta no puede contener scripts')
         return nombre_cuenta
+
+    def clean_tipo_cuenta(self):
+        tipo_cuenta = self.cleaned_data['tipo_cuenta']
+        choices = [choice[0] for choice in Cuenta.TipoCuenta.choices]
+        if tipo_cuenta not in choices:
+            raise forms.ValidationError('Tipo de cuenta no válido')
+        return tipo_cuenta
+
+    def clean(self):
+        cleaned_data = super().clean()
+        for field_name in cleaned_data:
+            value = cleaned_data[field_name]
+            if isinstance(value, str):
+                cleaned_data[field_name] = escape(value)
+            if any(char in value for char in
+                   [';', '--', "'", '"', '#', '/*', '*/', '<script>', '<img>', '<svg>', '<', '>', '(', ')']):
+                raise forms.ValidationError('Caracteres no permitidos en los campos')
+        return cleaned_data
+
+
+class MovimientoForm(forms.Form):
+
+    def __init__(self, origen, destino, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        choices_origen = [(cuenta.id, cuenta.nombre) for cuenta in origen]
+        self.fields['cuenta_origen'] = forms.ChoiceField(choices=choices_origen, label='Cuenta de origen')
+
+        choices_destino = [(cuenta.id, cuenta.nombre) for cuenta in destino]
+        self.fields['cuenta_destino'] = forms.ChoiceField(choices=choices_destino, label='Cuenta de destino')
+
+    monto = forms.FloatField(label='Monto')
+    concepto = forms.CharField(label='Concepto', widget=forms.Textarea)
+    fecha = forms.DateField(label='Fecha', widget=forms.DateInput(attrs={'type': 'date'}))
+
+    def clean_concepto(self):
+        concepto = self.cleaned_data['concepto']
+        if isinstance(concepto, str):
+            concepto = escape(concepto)
+        if any([char in concepto for char in
+                [';', '--', "'", '"', '#', '/*', '*/', '<script>', '<img>', '<svg>', '<', '>', '(', ')']]):
+            raise forms.ValidationError('El concepto no puede contener caracteres especiales')
+        return concepto
+
+    def clean_monto(self):
+        monto = self.cleaned_data['monto']
+        if not isinstance(monto, (int, float)) or monto <= 0:
+            raise forms.ValidationError('El monto debe ser un número mayor que cero')
+        return monto
+
+    def clean(self):
+        cleaned_data = super().clean()
+        cuenta_origen_id = cleaned_data.get('cuenta_origen')
+        cuenta_destino_id = cleaned_data.get('cuenta_destino')
+
+        cuentas_origen_ids = [cuenta.id for cuenta in Cuenta.objects.all()]
+        cuentas_destino_ids = [cuenta.id for cuenta in Cuenta.objects.all()]
+
+        if cuenta_origen_id not in cuentas_origen_ids:
+            raise forms.ValidationError('La cuenta de origen no es válida')
+
+        if cuenta_destino_id not in cuentas_destino_ids:
+            raise forms.ValidationError('La cuenta de destino no es válida')
--- cuentas/views.py
+++ cuentas/views.py
@@ -1,8 +1,9 @@
+from django.utils.html import escape
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET, require_POST, require_http_methods
 
-from .forms import CrearCuentaForm
+from .forms import CrearCuentaForm, MovimientoForm
 from .models import Cuenta, Transaccion
 from functools import reduce
 from logs.models import OperationLog
@@ -29,15 +30,15 @@ def index(request):
 @login_required
 @require_http_methods(["GET", "POST"])
 def cuenta_view(request):
-    form = CrearCuentaForm()
     op = request.GET.get("op")
+    form = CrearCuentaForm(request.POST)
+    message = ""
 
     if request.method == "POST":
-        form = CrearCuentaForm(request.POST)
         if form.is_valid():
             user = request.user
-            nombre = form.cleaned_data.get("nombre_cuenta")
-            tipo = form.cleaned_data.get("tipo_cuenta")
+            nombre = escape(form.cleaned_data.get("nombre_cuenta"))
+            tipo = escape(form.cleaned_data.get("tipo_cuenta"))
 
             cuenta = Cuenta.objects.using('default').create(
                 nombre=nombre,
@@ -54,12 +55,15 @@ def cuenta_view(request):
             )
 
             return redirect("/cuentas")
+        else:
+            message = "No se pudo crear la cuenta"
 
     context = {
         "form": form,
         "cuentas": Cuenta.objects.using('default').filter(propietario=request.user),
         "seleccion": "cuenta",
         "op": op,
+        "message": escape(message)
     }
 
     return render(request, "cuentas/cuentas.html", context=context)
@@ -70,50 +74,58 @@ def cuenta_view(request):
 def movimiento_view(request):
     op = request.GET.get("op", "")
     user = request.user
+    movimientos = Transaccion.objects.using('default').filter(origen__propietario__id=user.id)
+
+    form = None
+
+    if op == "ingreso":
+        cuentas_origen = Cuenta.objects.using('default').filter(tipo=Cuenta.TipoCuenta.INGRESO)
+        cuentas_destino = Cuenta.objects.using('default').filter(tipo=Cuenta.TipoCuenta.INGRESO_EGRESO)
+        form = MovimientoForm(cuentas_origen, cuentas_destino)
+
+    elif op == "gasto":
+        cuentas_origen = Cuenta.objects.using('default').filter(tipo=Cuenta.TipoCuenta.INGRESO_EGRESO)
+        cuentas_destino = Cuenta.objects.using('default').filter(tipo=Cuenta.TipoCuenta.GASTO)
+        form = MovimientoForm(cuentas_origen, cuentas_destino)
+
+    elif op == "traspaso":
+        cuentas_origen = Cuenta.objects.using('default').filter(tipo=Cuenta.TipoCuenta.INGRESO_EGRESO)
+        cuentas_destino = Cuenta.objects.using('default').filter(tipo=Cuenta.TipoCuenta.INGRESO_EGRESO)
+        form = MovimientoForm(cuentas_origen, cuentas_destino)
+
+    if request.method == "POST":
+        form = MovimientoForm(cuentas_origen, cuentas_destino, request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            origen_id = cleaned_data['cuenta_origen']
+            destino_id = cleaned_data['cuenta_destino']
+            cuenta_origen = Cuenta.objects.using('default').get(pk=origen_id)
+            cuenta_destino = Cuenta.objects.using('default').get(pk=destino_id)
+            transaccion = Transaccion.objects.using('default').create(
+                origen=cuenta_origen,
+                destino=cuenta_destino,
+                fecha=cleaned_data['fecha'],
+                monto=cleaned_data['monto'],
+                concepto=cleaned_data['concepto']
+            )
+            transaccion.realizar()
+
+            OperationLog.objects.using('logsdb').create(
+                user_id=user.id,
+                user_username=user.username,
+                operation=f"Nueva transaccion. ID: {transaccion.id}"
+            )
+
+            movimientos = Transaccion.objects.using('default').filter(origen__propietario__id=user.id)
+            return redirect("/movimientos")
 
     context = {
         "op": op,
-        "movimientos": Transaccion.objects.using('default').filter(origen__propietario__id=user.id),
-        "seleccion": "movimiento"
+        "movimientos": movimientos,
+        "seleccion": "movimiento",
+        "form": form
     }
 
-    if op == "ingreso":
-        context["cuentas_origen"] = Cuenta.objects.filter(tipo=Cuenta.TipoCuenta.INGRESO)
-        context["cuentas_destino"] = Cuenta.objects.filter(tipo=Cuenta.TipoCuenta.INGRESO_EGRESO)
-
-    if op == "gasto":
-        context["cuentas_origen"] = Cuenta.objects.filter(tipo=Cuenta.TipoCuenta.INGRESO_EGRESO)
-        context["cuentas_destino"] = Cuenta.objects.filter(tipo=Cuenta.TipoCuenta.GASTO)
-
-    if op == "traspaso":
-        context["cuentas_origen"] = Cuenta.objects.filter(tipo=Cuenta.TipoCuenta.INGRESO_EGRESO)
-        context["cuentas_destino"] = Cuenta.objects.filter(tipo=Cuenta.TipoCuenta.INGRESO_EGRESO)
-
-    if request.method == "POST":
-        cuenta_origen = request.POST.get("cuenta_origen")
-        cuenta_destino = request.POST.get("cuenta_destino")
-        monto = request.POST.get("monto")
-        concepto = request.POST.get("concepto")
-        fecha = request.POST.get("fecha")
-
-        origen = Cuenta.objects.using('default').get(id=cuenta_origen)
-        destino = Cuenta.objects.using('default').get(id=cuenta_destino)
-
-        transaccion = Transaccion()
-        transaccion.origen = origen
-        transaccion.destino = destino
-        transaccion.monto = float(monto)
-        transaccion.concepto = concepto
-        transaccion.fecha = fecha
-
-        transaccion.realizar()
-
-        OperationLog.objects.using('logsdb').create(
-            user_id=user.id,
-            user_username=user.username,
-            operation="Nueva transaccion. ID: " + str(transaccion.id)
-        )
-
-        return redirect("/movimientos")
-
     return render(request, "movimientos/movimientos.html", context=context)
+
+
